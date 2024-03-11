7114bd800787bd41ac49bc58861484ff906e50ae
João Góes <goes.joaoseje@gmail.com>
2024-03-11 20:49:33 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -34,12 +34,20 @@ public class Board {
 	}
 	
 	public void placePiece(Piece piece, Position position) {
-		if (thereIsAPiece(position)) {
-			throw new BoardException("There is already a piece on position " + position);
-		}
+		thereIsAPieceException(position);
 		pieces[position.getRow()][position.getColumn()] = piece;
 	}
 	
+	public Piece removePiece(Position position) {
+		positionExistsException(position);
+		
+		if (piece(position) == null) return null;
+		
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
 	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
@@ -54,13 +62,19 @@ public class Board {
 		return piece(position)!= null;
 	}
 	
-	private void positionExistsException(Position position) {
+	public void thereIsAPieceException(Position position) {
+		if(thereIsAPiece(position) ) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
+	}
+	
+	public void positionExistsException(Position position) {
 		if(!positionExists(position) ) {
 			throw new BoardException("Position is not on the board");
 		}
 	}
 	
-	private void positionExistsException(int row, int column) {
+	public void positionExistsException(int row, int column) {
 		if(!positionExists(row, column) ) {
 			throw new BoardException("Position is not on the board");
 		}
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,11 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
+import chess.pieces.Knight;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -23,24 +27,47 @@ public class ChessMatch {
 		return mat;
 	}
 	
-	private void placeNewPiece(char column, int row, ChessPiece piece) {
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece)capturedPiece;
+	}
+	
+	private void validateSourcePosition(Position positio) {
+		if (!board.thereIsAPiece(positio)) {
+			throw new ChessException("There is no piece on source position");
+		}
+	}
+	
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(target);
+		board.placePiece(p, target);
+		return capturedPiece;
+	}
+	
+	private void placeNewPiece(ChessPiece piece, char column, int row) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
 	
 	private void initialSetup() {
-		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('c', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 2, new Rook(board, Color.WHITE));
-        placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new King(board, Color.WHITE));
+		placeNewPiece(new Rook(board, Color.WHITE), 'a', 1);
+        placeNewPiece(new Rook(board, Color.WHITE), 'h', 1);
+        placeNewPiece(new Knight(board, Color.WHITE),'b', 1 );
+        placeNewPiece(new Knight(board, Color.WHITE), 'g', 1);
+        placeNewPiece(new Bishop(board, Color.WHITE), 'c', 1);
+        placeNewPiece(new Bishop(board, Color.WHITE), 'f', 1);
+        placeNewPiece(new King(board, Color.WHITE), 'e', 1);
 
-        placeNewPiece('c', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('c', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 7, new Rook(board, Color.BLACK));
-        placeNewPiece('e', 8, new Rook(board, Color.BLACK));
-        placeNewPiece('d', 8, new King(board, Color.BLACK));
+        placeNewPiece(new Rook(board, Color.BLACK), 'a', 8);
+        placeNewPiece(new Rook(board, Color.BLACK), 'h', 8);
+        placeNewPiece(new Rook(board, Color.BLACK), 'b', 8);
+        placeNewPiece(new Rook(board, Color.BLACK), 'g', 8);
+        placeNewPiece(new Bishop(board, Color.BLACK), 'c', 8);
+        placeNewPiece(new Bishop(board, Color.BLACK), 'f', 8);
+        placeNewPiece(new King(board, Color.BLACK), 'e', 8);
 		
 	}
  }
